0af40bb6a8e8e430f860bb157786da048d657741
Low-Key JC <79029972+jcahsy170324@users.noreply.github.com>
2022-03-10 14:51:41 +0800

    mybatis-plus代码生成器生成代码

---
+++ msb-dongbao-mall-service/msb-dongbao-ums/src/main/java/com/msb/dongbao/ums/entity/UmsMember.java
@@ -0,0 +1,75 @@
+package com.msb.dongbao.ums.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 后台用户表
+ * </p>
+ *
+ * @author JinCheng
+ * @since 2022-03-10
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class UmsMember implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    /**
+     * 头像
+     */
+    private String icon;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 昵称
+     */
+    private String nickName;
+
+    /**
+     * 备注信息
+     */
+    private String note;
+
+    /**
+     * 创建时间
+     */
+    private Date gmtCreate;
+
+    /**
+     * 最后登录时间
+     */
+    private Date loginTime;
+
+    /**
+     * 帐号启用状态：0->禁用；1->启用
+     */
+    private Integer status;
+
+    /**
+     * 更新时间
+     */
+    private Date gmtUpdate;
+
+
+}
---
+++ msb-dongbao-mall-service/msb-dongbao-ums/src/main/java/com/msb/dongbao/ums/service/UmsMemberService.java
@@ -0,0 +1,16 @@
+package com.msb.dongbao.ums.service;
+
+import com.msb.dongbao.ums.entity.UmsMember;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 后台用户表 服务类
+ * </p>
+ *
+ * @author JinCheng
+ * @since 2022-03-10
+ */
+public interface UmsMemberService extends IService<UmsMember> {
+
+}
